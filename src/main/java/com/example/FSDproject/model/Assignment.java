c08daf9a59ecb06b53c6d61c76320bb7b3b39d85
Lekhyasree-12 <2400031881@kluniversity.in>
2026-04-28 12:26:03 +0530

    changes made

--- src/main/java/com/example/FSDproject/model/Assignment.java
+++ src/main/java/com/example/FSDproject/model/Assignment.java
@@ -11,6 +11,9 @@ public class Assignment {
 
     private String title;
     private String description;
+    private String dueDate;
+    private Integer points;
+    private Long subjectId;
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -20,4 +23,13 @@ public class Assignment {
 
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
+
+    public String getDueDate() { return dueDate; }
+    public void setDueDate(String dueDate) { this.dueDate = dueDate; }
+
+    public Integer getPoints() { return points; }
+    public void setPoints(Integer points) { this.points = points; }
+
+    public Long getSubjectId() { return subjectId; }
+    public void setSubjectId(Long subjectId) { this.subjectId = subjectId; }
 }
